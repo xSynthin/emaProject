460e0e6c49f760dc209a750c0d7c9dc609b37c39
Synth <piplup654@protonmail.com>
2022-09-21 15:41:37 +0200

    Dodałem playerManager oraz GameManager

--- Assets/Scripts/Singletons/GManager.cs
+++ Assets/Scripts/Singletons/GManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,14 @@ using UnityEngine;
 public class GManager : MonoBehaviour
 {
     public static GManager instance;
+    public event Action onGameStartEvent;
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        onGameStartEvent?.Invoke();
     }
 }
---
+++ Assets/Scripts/Singletons/PlayerManager.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance;
+    [SerializeField] public PlayerUtils playerUtils;
+    public event Action playerSpeedBoostActivateEvent;
+    public event Action playerSpeedBoostDeactivateEvent;
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void CallPlayerSpeedBoostActivateEvent() => playerSpeedBoostActivateEvent?.Invoke();
+    public void CallPlayerSpeedBoostDeactivateEvent() => playerSpeedBoostDeactivateEvent?.Invoke();
+}
